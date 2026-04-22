f8eefdaffdbac93e41deb034db81c63ea1193ea0
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-04-22 14:15:50 -0500

    FIXED BUGS

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // ==========================================
 // IMPORTACIONES
 // ==========================================
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
