1bebc3c26c0dc1b4f83dd7969dd84c1299a171e0
MiguelMartinez32143 <miguelmartinezvaron32143@gmail.com>
2026-04-23 15:06:39 -0500

    Cambio URL FrontENd

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // IMPORTANTE: Reemplaza esta URL por la URL pública que te dio Railway (sin el / al final)
   // Ejemplo: 'https://backend-gestor-tareas-production.up.railway.app'
-  apiUrl: 'https://tareasgestor-production.up.railway.app'
+  apiUrl: 'https://tareas-gestor-backend.onrender.com'
 };
